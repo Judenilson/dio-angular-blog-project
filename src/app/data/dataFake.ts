efe5c2962b8800e911fc97482569bdce45f50c46
judenilson <judenilson@hotmail.com>
2022-12-07 23:50:59 -0300

    blog ok

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -3,42 +3,91 @@ export const dataFake = [
     "id": "1",
     "photoCover": "../../../assets/imagens/a123456.jpg",
     "title": "Linha 4080 difícil vender!",
-    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates amet illum nihil fuga at ut ad nulla aspernatur. Atque placeat doloribus distinctio dicta ipsa debitis"
+    "description": `Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur aut fugit quas ipsum, cumque, soluta dolores minus porro pariatur repellat non quia sed ratione laboriosam eveniet. Quam dignissimos quibusdam doloribus!
+    Exercitationem dolorem et cumque, excepturi fuga, ipsam officiis reprehenderit nisi atque blanditiis soluta velit, enim voluptatem quia quo odio alias pariatur rem illo delectus nesciunt dolorum harum. Suscipit, ut veritatis.
+    Ratione, totam, ea quisquam asperiores temporibus, ipsa adipisci porro aliquam fugit sint neque! Aliquam beatae sequi velit, perferendis maxime praesentium a optio voluptatibus, atque eaque eum cupiditate omnis cum magni?
+    Architecto laborum in ullam, facere inventore fugit dolor ducimus odio sed quae accusamus tempora voluptatem quam, sit enim pariatur eligendi deserunt voluptatibus! Nulla dolore quae cum, tempora autem culpa corporis.
+    Voluptatum reiciendis aliquid vero velit, id eveniet placeat, delectus dolore quod quas laboriosam nam laudantium? Sint adipisci ut suscipit, molestiae atque delectus? Eligendi quisquam soluta voluptatum harum minus, accusamus mollitia!
+    Veniam, minima reiciendis laudantium facere accusamus assumenda provident dolorem nesciunt explicabo expedita, exercitationem beatae consectetur numquam ipsum quisquam perferendis enim repudiandae? Distinctio consectetur obcaecati sapiente doloribus vitae sint provident illo!
+    Natus debitis, obcaecati reiciendis aliquam repellendus quibusdam aperiam velit odit ea voluptates sed sit eveniet laboriosam molestiae architecto modi ad nam dolorem eligendi voluptatibus. Neque expedita libero ea laudantium blanditiis?
+    Iusto nam ipsa labore eos corporis id iste maiores minima voluptatibus cum suscipit minus blanditiis eligendi, maxime harum quia, soluta repellat. Provident dicta distinctio quo ipsum ratione ex non eum.`
   },
   {
     "id": "2",
     "photoCover": "../../../assets/imagens/a122.jpg",
     "title": "O metaverso é o futuro?",
-    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita incidunt placeat praesentium rem, debitis nam in a qui facilis dolorum ea amet quia vero! Magnam blanditiis laudantium ipsam a vitae..."
+    "description": `Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur aut fugit quas ipsum, cumque, soluta dolores minus porro pariatur repellat non quia sed ratione laboriosam eveniet. Quam dignissimos quibusdam doloribus!
+    Exercitationem dolorem et cumque, excepturi fuga, ipsam officiis reprehenderit nisi atque blanditiis soluta velit, enim voluptatem quia quo odio alias pariatur rem illo delectus nesciunt dolorum harum. Suscipit, ut veritatis.
+    Ratione, totam, ea quisquam asperiores temporibus, ipsa adipisci porro aliquam fugit sint neque! Aliquam beatae sequi velit, perferendis maxime praesentium a optio voluptatibus, atque eaque eum cupiditate omnis cum magni?
+    Architecto laborum in ullam, facere inventore fugit dolor ducimus odio sed quae accusamus tempora voluptatem quam, sit enim pariatur eligendi deserunt voluptatibus! Nulla dolore quae cum, tempora autem culpa corporis.
+    Voluptatum reiciendis aliquid vero velit, id eveniet placeat, delectus dolore quod quas laboriosam nam laudantium? Sint adipisci ut suscipit, molestiae atque delectus? Eligendi quisquam soluta voluptatum harum minus, accusamus mollitia!
+    Veniam, minima reiciendis laudantium facere accusamus assumenda provident dolorem nesciunt explicabo expedita, exercitationem beatae consectetur numquam ipsum quisquam perferendis enim repudiandae? Distinctio consectetur obcaecati sapiente doloribus vitae sint provident illo!
+    Natus debitis, obcaecati reiciendis aliquam repellendus quibusdam aperiam velit odit ea voluptates sed sit eveniet laboriosam molestiae architecto modi ad nam dolorem eligendi voluptatibus. Neque expedita libero ea laudantium blanditiis?
+    Iusto nam ipsa labore eos corporis id iste maiores minima voluptatibus cum suscipit minus blanditiis eligendi, maxime harum quia, soluta repellat. Provident dicta distinctio quo ipsum ratione ex non eum.`
   },
   {
     "id": "3",
     "photoCover": "../../../assets/imagens/a3.jpg",
     "title": "Java Script será a linguagem eterna",
-    "description": "Accusamus recusandae facere dicta, nesciunt quisquam rem quod! Quo earum delectus placeat, blanditiis natus ratione facilis ad nam amet accusantium, mollitia pariatur, eos quod iste alias ab sit eius distinctio..."
+    "description": `Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur aut fugit quas ipsum, cumque, soluta dolores minus porro pariatur repellat non quia sed ratione laboriosam eveniet. Quam dignissimos quibusdam doloribus!
+    Exercitationem dolorem et cumque, excepturi fuga, ipsam officiis reprehenderit nisi atque blanditiis soluta velit, enim voluptatem quia quo odio alias pariatur rem illo delectus nesciunt dolorum harum. Suscipit, ut veritatis.
+    Ratione, totam, ea quisquam asperiores temporibus, ipsa adipisci porro aliquam fugit sint neque! Aliquam beatae sequi velit, perferendis maxime praesentium a optio voluptatibus, atque eaque eum cupiditate omnis cum magni?
+    Architecto laborum in ullam, facere inventore fugit dolor ducimus odio sed quae accusamus tempora voluptatem quam, sit enim pariatur eligendi deserunt voluptatibus! Nulla dolore quae cum, tempora autem culpa corporis.
+    Voluptatum reiciendis aliquid vero velit, id eveniet placeat, delectus dolore quod quas laboriosam nam laudantium? Sint adipisci ut suscipit, molestiae atque delectus? Eligendi quisquam soluta voluptatum harum minus, accusamus mollitia!
+    Veniam, minima reiciendis laudantium facere accusamus assumenda provident dolorem nesciunt explicabo expedita, exercitationem beatae consectetur numquam ipsum quisquam perferendis enim repudiandae? Distinctio consectetur obcaecati sapiente doloribus vitae sint provident illo!
+    Natus debitis, obcaecati reiciendis aliquam repellendus quibusdam aperiam velit odit ea voluptates sed sit eveniet laboriosam molestiae architecto modi ad nam dolorem eligendi voluptatibus. Neque expedita libero ea laudantium blanditiis?
+    Iusto nam ipsa labore eos corporis id iste maiores minima voluptatibus cum suscipit minus blanditiis eligendi, maxime harum quia, soluta repellat. Provident dicta distinctio quo ipsum ratione ex non eum.`
   },
   {
     "id": "4",
     "photoCover": "../../../assets/imagens/a124.jpg",
     "title": "Como os robôs ajudam atualmente",
-    "description": "Eius quaerat, adipisci architecto ut iste suscipit aperiam debitis facilis odit, dolores, quis fuga quo corrupti? Vel, quis sed quia, deleniti blanditiis, molestiae sapiente voluptas ab assumenda voluptatem obcaecati optio..."
+    "description": `Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur aut fugit quas ipsum, cumque, soluta dolores minus porro pariatur repellat non quia sed ratione laboriosam eveniet. Quam dignissimos quibusdam doloribus!
+    Exercitationem dolorem et cumque, excepturi fuga, ipsam officiis reprehenderit nisi atque blanditiis soluta velit, enim voluptatem quia quo odio alias pariatur rem illo delectus nesciunt dolorum harum. Suscipit, ut veritatis.
+    Ratione, totam, ea quisquam asperiores temporibus, ipsa adipisci porro aliquam fugit sint neque! Aliquam beatae sequi velit, perferendis maxime praesentium a optio voluptatibus, atque eaque eum cupiditate omnis cum magni?
+    Architecto laborum in ullam, facere inventore fugit dolor ducimus odio sed quae accusamus tempora voluptatem quam, sit enim pariatur eligendi deserunt voluptatibus! Nulla dolore quae cum, tempora autem culpa corporis.
+    Voluptatum reiciendis aliquid vero velit, id eveniet placeat, delectus dolore quod quas laboriosam nam laudantium? Sint adipisci ut suscipit, molestiae atque delectus? Eligendi quisquam soluta voluptatum harum minus, accusamus mollitia!
+    Veniam, minima reiciendis laudantium facere accusamus assumenda provident dolorem nesciunt explicabo expedita, exercitationem beatae consectetur numquam ipsum quisquam perferendis enim repudiandae? Distinctio consectetur obcaecati sapiente doloribus vitae sint provident illo!
+    Natus debitis, obcaecati reiciendis aliquam repellendus quibusdam aperiam velit odit ea voluptates sed sit eveniet laboriosam molestiae architecto modi ad nam dolorem eligendi voluptatibus. Neque expedita libero ea laudantium blanditiis?
+    Iusto nam ipsa labore eos corporis id iste maiores minima voluptatibus cum suscipit minus blanditiis eligendi, maxime harum quia, soluta repellat. Provident dicta distinctio quo ipsum ratione ex non eum.`
   },
   {
     "id": "5",
     "photoCover": "../../../assets/imagens/a1.jpg",
     "title": "Eletrônica digital para você",
-    "description": "Eius quaerat, adipisci architecto ut iste suscipit aperiam debitis facilis odit, dolores, quis fuga quo corrupti? Vel, quis sed quia, deleniti blanditiis, molestiae sapiente voluptas ab assumenda voluptatem obcaecati optio..."
+    "description": `Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur aut fugit quas ipsum, cumque, soluta dolores minus porro pariatur repellat non quia sed ratione laboriosam eveniet. Quam dignissimos quibusdam doloribus!
+    Exercitationem dolorem et cumque, excepturi fuga, ipsam officiis reprehenderit nisi atque blanditiis soluta velit, enim voluptatem quia quo odio alias pariatur rem illo delectus nesciunt dolorum harum. Suscipit, ut veritatis.
+    Ratione, totam, ea quisquam asperiores temporibus, ipsa adipisci porro aliquam fugit sint neque! Aliquam beatae sequi velit, perferendis maxime praesentium a optio voluptatibus, atque eaque eum cupiditate omnis cum magni?
+    Architecto laborum in ullam, facere inventore fugit dolor ducimus odio sed quae accusamus tempora voluptatem quam, sit enim pariatur eligendi deserunt voluptatibus! Nulla dolore quae cum, tempora autem culpa corporis.
+    Voluptatum reiciendis aliquid vero velit, id eveniet placeat, delectus dolore quod quas laboriosam nam laudantium? Sint adipisci ut suscipit, molestiae atque delectus? Eligendi quisquam soluta voluptatum harum minus, accusamus mollitia!
+    Veniam, minima reiciendis laudantium facere accusamus assumenda provident dolorem nesciunt explicabo expedita, exercitationem beatae consectetur numquam ipsum quisquam perferendis enim repudiandae? Distinctio consectetur obcaecati sapiente doloribus vitae sint provident illo!
+    Natus debitis, obcaecati reiciendis aliquam repellendus quibusdam aperiam velit odit ea voluptates sed sit eveniet laboriosam molestiae architecto modi ad nam dolorem eligendi voluptatibus. Neque expedita libero ea laudantium blanditiis?
+    Iusto nam ipsa labore eos corporis id iste maiores minima voluptatibus cum suscipit minus blanditiis eligendi, maxime harum quia, soluta repellat. Provident dicta distinctio quo ipsum ratione ex non eum.`
   },
   {
     "id": "6",
     "photoCover": "../../../assets/imagens/a2.jpg",
     "title": "Humanóides aposta da Tesla",
-    "description": "Exercitationem amet dignissimos consequuntur, eligendi, dolorum soluta fuga id tempora quis dolorem deserunt facere explicabo? Assumenda, exercitationem facilis earum maxime, cum porro repellat, rerum beatae eveniet dolores quidem libero consectetur..."
+    "description": `Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur aut fugit quas ipsum, cumque, soluta dolores minus porro pariatur repellat non quia sed ratione laboriosam eveniet. Quam dignissimos quibusdam doloribus!
+    Exercitationem dolorem et cumque, excepturi fuga, ipsam officiis reprehenderit nisi atque blanditiis soluta velit, enim voluptatem quia quo odio alias pariatur rem illo delectus nesciunt dolorum harum. Suscipit, ut veritatis.
+    Ratione, totam, ea quisquam asperiores temporibus, ipsa adipisci porro aliquam fugit sint neque! Aliquam beatae sequi velit, perferendis maxime praesentium a optio voluptatibus, atque eaque eum cupiditate omnis cum magni?
+    Architecto laborum in ullam, facere inventore fugit dolor ducimus odio sed quae accusamus tempora voluptatem quam, sit enim pariatur eligendi deserunt voluptatibus! Nulla dolore quae cum, tempora autem culpa corporis.
+    Voluptatum reiciendis aliquid vero velit, id eveniet placeat, delectus dolore quod quas laboriosam nam laudantium? Sint adipisci ut suscipit, molestiae atque delectus? Eligendi quisquam soluta voluptatum harum minus, accusamus mollitia!
+    Veniam, minima reiciendis laudantium facere accusamus assumenda provident dolorem nesciunt explicabo expedita, exercitationem beatae consectetur numquam ipsum quisquam perferendis enim repudiandae? Distinctio consectetur obcaecati sapiente doloribus vitae sint provident illo!
+    Natus debitis, obcaecati reiciendis aliquam repellendus quibusdam aperiam velit odit ea voluptates sed sit eveniet laboriosam molestiae architecto modi ad nam dolorem eligendi voluptatibus. Neque expedita libero ea laudantium blanditiis?
+    Iusto nam ipsa labore eos corporis id iste maiores minima voluptatibus cum suscipit minus blanditiis eligendi, maxime harum quia, soluta repellat. Provident dicta distinctio quo ipsum ratione ex non eum.`
   },
   {
     "id": "7",
     "photoCover": "../../../assets/imagens/a123.jpg",
     "title": "Inteligência Artificial, a treta",
-    "description": "Minus non ad officiis facilis? Nulla, vel. Ad asperiores minima magni accusantium odio eius ipsam dolore velit blanditiis? Vero sint quos eaque harum vitae, doloribus possimus voluptates placeat impedit nulla..."
+    "description": `Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur aut fugit quas ipsum, cumque, soluta dolores minus porro pariatur repellat non quia sed ratione laboriosam eveniet. Quam dignissimos quibusdam doloribus!
+    Exercitationem dolorem et cumque, excepturi fuga, ipsam officiis reprehenderit nisi atque blanditiis soluta velit, enim voluptatem quia quo odio alias pariatur rem illo delectus nesciunt dolorum harum. Suscipit, ut veritatis.
+    Ratione, totam, ea quisquam asperiores temporibus, ipsa adipisci porro aliquam fugit sint neque! Aliquam beatae sequi velit, perferendis maxime praesentium a optio voluptatibus, atque eaque eum cupiditate omnis cum magni?
+    Architecto laborum in ullam, facere inventore fugit dolor ducimus odio sed quae accusamus tempora voluptatem quam, sit enim pariatur eligendi deserunt voluptatibus! Nulla dolore quae cum, tempora autem culpa corporis.
+    Voluptatum reiciendis aliquid vero velit, id eveniet placeat, delectus dolore quod quas laboriosam nam laudantium? Sint adipisci ut suscipit, molestiae atque delectus? Eligendi quisquam soluta voluptatum harum minus, accusamus mollitia!
+    Veniam, minima reiciendis laudantium facere accusamus assumenda provident dolorem nesciunt explicabo expedita, exercitationem beatae consectetur numquam ipsum quisquam perferendis enim repudiandae? Distinctio consectetur obcaecati sapiente doloribus vitae sint provident illo!
+    Natus debitis, obcaecati reiciendis aliquam repellendus quibusdam aperiam velit odit ea voluptates sed sit eveniet laboriosam molestiae architecto modi ad nam dolorem eligendi voluptatibus. Neque expedita libero ea laudantium blanditiis?
+    Iusto nam ipsa labore eos corporis id iste maiores minima voluptatibus cum suscipit minus blanditiis eligendi, maxime harum quia, soluta repellat. Provident dicta distinctio quo ipsum ratione ex non eum.`
   }
 ]
